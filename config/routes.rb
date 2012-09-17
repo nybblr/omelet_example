7b1693ad44d9ff95334203d26cd44b25b4b9eb06
Jonathan Martin <me@nybblr.com>
2012-09-17 15:40:38 -0400

    Upgraded version of omelet_ui and add engine mount. We're up!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 OmeletExample::Application.routes.draw do
   devise_for :users
+	mount OmeletUi::Engine => "/omelet"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
