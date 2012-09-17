ef3cd0eb62a188d005c9cd96699eaf7fcea7e0aa
Jonathan Martin <me@nybblr.com>
2012-09-17 13:11:40 -0400

    Added our gem omelet-ui!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmeletExample::Application.config.secret_token = '895ecffaf9362024ebf981cceb444b57ffd2a1e28d086c5c383d653417fed385b02828c38a9c999249710d25c3e474485b980c8de212402a1db6d7a77b80f31c'
